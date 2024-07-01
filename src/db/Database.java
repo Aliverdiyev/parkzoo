11927d37cd1d54570b2338ec07f365c757e09b6d
Aliverdiyev <aliverdiyev00@gmail.com>
2024-07-01 16:54:38 +0400

    change database

--- src/db/Database.java
+++ src/db/Database.java
@@ -6,10 +6,15 @@ import model.TerrestrialCarnivorous;
 import model.TerrestrialNonCarnivorous;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
-    public static ArrayList<AquaticCarnivorous> aquaticCarnivorous = new ArrayList<>();
-    public static ArrayList<AquaticNonCarnivorous> aquaticNonCarnivorous = new ArrayList<>();
-    public static ArrayList<TerrestrialCarnivorous> terrestrialCarnivorous = new ArrayList<>();
-    public static ArrayList<TerrestrialNonCarnivorous> terrestrialNonCarnivorous = new ArrayList<>();
+    public static final List<AquaticCarnivorous> aquaticCarnivorous = new ArrayList<>();
+    public static final List<AquaticNonCarnivorous> aquaticNonCarnivorous = new ArrayList<>();
+    public static final List<TerrestrialCarnivorous> terrestrialCarnivorous = new ArrayList<>();
+    public static final List<TerrestrialNonCarnivorous> terrestrialNonCarnivorous = new ArrayList<>();
+
+    private Database() {
+
+    }
 }
